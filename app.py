9d5a8c181e8a5ba6d98d224f811bc65e91467017
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-07-31 20:43:05 -0400

    work

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
-=======
-from flask import Flask, render_template, request, redirect
-from flask_dropzone import Dropzone
->>>>>>> bd0f5c5214ae0be8624de9b262db073693381f73
 import os
 
 app = Flask(__name__)
@@ -11,11 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('construction.html')
-    # return render_template('home.html')
+    # return render_template('construction.html')
+     return render_template('home.html')
 
-<<<<<<< HEAD
-=======
 @app.route("/hbc")
 def hbc():
     return render_template('hbc.html')
@@ -31,6 +24,5 @@ def upload():
 def github():
     return redirect("https://github.com/pranavaddepalli/Laire", code=302)
 
->>>>>>> bd0f5c5214ae0be8624de9b262db073693381f73
 if __name__ == '__main__':
     app.run(debug=True)
