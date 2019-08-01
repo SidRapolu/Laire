c725426ab543c1e98bb4a21d542c3f0ecb4fed4f
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-07-31 20:05:15 -0400

    updated app.py and added sid's sectioning code

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-from flask import Flask, render_template, request
-from flask_dropzone import Dropzone
+from flask import Flask, render_template
 import os
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['UPLOADED_PATH'] = os.getcwd() + '/uploads'
-dropzone = Dropzone(app)
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
-
-@app.route("/hbc")
-def hbc():
-    return render_template('hbc.html')
-
-@app.route("/upload", methods=['POST', 'GET'])
-def upload():
-    if request.method == 'POST':
-        for f in request.files.getlist('file'):
-            f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    return render_template('hbc.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
