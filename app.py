97efd2ca8d074828b3c80adc5f10c3c966b5a66d
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-07-31 21:58:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     # return render_template('construction.html')
-     return render_template('home.html')
-
-@app.route("/hbc")
-def hbc():
-    return render_template('hbc.html')
-
-@app.route("/upload", methods=['POST', 'GET'])
-def upload():
-    if request.method == 'POST':
-        for f in request.files.getlist('file'):
-            f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    return render_template('hbc.html')
+     return render_template('construction.html')
+     # return render_template('home.html')
 
 @app.route("/github")
 def github():
