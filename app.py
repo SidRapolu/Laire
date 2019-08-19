5f8ff7173f1e6e244d724d7708bc8cba25125e99
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-08-18 23:13:03 -0400

    added contact form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -24,6 +24,25 @@ def sid():
 @app.route("/haneef")
 def haneef():
     return render_template('haneef.html')
+
+app.config.update(dict(
+    MAIL_SERVER = 'smtp.domain.com',
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'hello@laire.org',
+    MAIL_PASSWORD = 'qwertyuiopA123!'
+))
+mail = Mail(app)
+
+@app.route('/contact_email', methods=['POST'])
+def contact_email():
+    print(request.form['Name'])
+    msg = Message('Contact From Website', sender='hello@laire.org', recipients=['info.laire@gmail.com'])
+    msg.body = 'Name: ' + request.form['Name'] + '\n' + 'Subject: ' + request.form['Subject'] + '\n' + 'Message: ' + request.form['Message'] + '\n' + 'Email: ' + request.form['Email'] #Customize based on user input
+
+    mail.send(msg)
+    return redirect("/#contact")
 
 if __name__ == '__main__':
     app.run(debug=True)
